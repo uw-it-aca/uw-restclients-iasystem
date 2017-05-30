1e37c9d4722d45baf44cf1804d3fe6820b2d7b63
Ethan Turner <ezturner@washington.edu>
2017-05-30 14:41:27 -0700

    Updated setup.py with pytz, added travis.yml

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
     author="UW-IT AXDD",
     author_email="aca-it@uw.edu",
     include_package_data=True,
-    install_requires=['UW-RestClients-Core>0.8,<1.0'
+    install_requires=['UW-RestClients-Core>0.8,<1.0',
+                      'pytz'
                      ],
     license='Apache License, Version 2.0',
     description=('A library for connecting to the Bridge API'),
