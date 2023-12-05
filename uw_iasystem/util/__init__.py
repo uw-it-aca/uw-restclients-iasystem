c4c3efa9c5091f7494bdd5f36a80977e13f1d70d
Fang Lin <fanglin@uw.edu>
2023-12-05 11:51:52 -0800

    Upgrade python and action versions

--- uw_iasystem/util/__init__.py
+++ uw_iasystem/util/__init__.py
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from restclients_core.util.decorators import use_mock
-from uw_iasystem.dao import IASystem_UW_DAO, IASystem_UWB_DAO,\
-    IASystem_UWT_DAO, IASystem_UWEO_AP_DAO, IASystem_UWEO_IELP_DAO
+from uw_iasystem.dao import (
+    IASystem_UW_DAO, IASystem_UWB_DAO,
+    IASystem_UWT_DAO, IASystem_UWEO_AP_DAO, IASystem_UWEO_IELP_DAO)
 
 
 fdao_ias_override = use_mock(IASystem_UW_DAO(), IASystem_UWB_DAO(),
