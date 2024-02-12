5dae3ed635c0d7a716c0e99923fe4912a2e4cbe9
Fang Lin <fanglin@uw.edu>
2024-02-12 09:29:09 -0800

    support python 3.10

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='UW-RestClients-IASystem',
     version=VERSION,
     packages=['uw_iasystem'],
-    author="UW-IT AXDD",
+    author="UW-IT T&LS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=['UW-RestClients-Core',
@@ -36,6 +36,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
     ],
 )
